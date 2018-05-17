dbb1cee7e5ccec1dc9d1f1112264fa7c9688a91a
Pedro Padilha <pedropadilha@me.com>
2018-05-17 04:10:14 -0300

    First commit - added all files

---
+++ main.js
@@ -0,0 +1,71 @@
+"use strict";
+
+const five = require("johnny-five");
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+let strip = null;
+
+app.use(express.static(__dirname + "/public"))
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html")
+});
+
+five.Board({
+	port: "COM9"
+}).on("ready", function() {
+
+  console.log("Arduino connected!");
+  let state = {
+		red: 0, green: 0, blue: 0
+	};
+
+   strip = new five.Led.RGB({
+   	pins: {
+      	red: 6,
+      	green: 9,
+      	blue: 3
+    	}
+   });
+
+   let setColors = function(state) {
+   	strip.color({
+      	red: state.red,
+      	blue: state.blue,
+      	green: state.green
+    	});
+  	};
+
+  	io.on("connection", function(client) {
+		client.on("join", function(handshake) {
+    	console.log(handshake);
+   });
+
+   setColors(state);
+
+   client.on("rgb", function(data) {
+		if (!data.get) {
+ 			state.red = data.red;
+      	state.green = data.green;
+    		state.blue = data.blue;
+			setColors(state);
+		}
+
+   	client.emit("rgb", state);
+   	client.broadcast.emit("rgb", state);
+   });
+
+});
+
+	this.repl.inject({
+		strip: strip
+	});
+
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port);
+console.log("Server listening on http://localhost:" + port);
---
+++ public/assets/js/client.js
@@ -0,0 +1,118 @@
+ "use strict";
+ var socket = io.connect(window.location.hostname + ":" + 3000);
+
+ $("input[type=\"range\"]").change(function () {
+	 socket.emit("rgb", {
+		 red: $("#red").val(),
+		 green: $("#green").val(),
+		 blue: $("#blue").val()
+	 });
+ });
+
+ $("#test").change(function() {
+	 var total = parseInt($(this).val());
+	 var color = toColor(total);
+
+	 socket.emit("rgb", color);
+
+ });
+
+ $("#viewColor").on("click", function () {
+ 	$("#picker2").toggle();
+ });
+
+ socket.on("connect", function(data) {
+     socket.emit("join", "Client is connected!");
+ });
+
+ socket.on("rgb", function(data) {
+	  var pickers = $("input[type=\"range\"]");
+	  $(pickers[0]).val(data.red);
+	  $(pickers[1]).val(data.green);
+	  $(pickers[2]).val(data.blue);
+	  var number = toNumber(data);
+	  $(pickers[3]).val(number || 0);
+	  changeViewColor(data);
+	  $("body").removeClass("loading");
+ });
+
+$(document).ready(function () {
+	$("body").addClass("loading");
+	socket.emit("rgb", {
+		get: true
+	});
+});
+
+function changeViewColor(data) {
+	var r = parseInt(data.red).toString(16);
+	var g = parseInt(data.green).toString(16);
+	var b = parseInt(data.blue).toString(16);
+	var color = "rgb(" + data.red + ", " + data.green + ", " + data.blue + ")";
+	$("#viewColor").css("background-color", color);
+}
+
+function toColor(total) {
+	var values = [];
+
+	for (var x = 0; x < 6; x++) {
+		if (total > 255) {
+			values[x] = 255;
+			total -= 255;
+		} else {
+			values[x] = total;
+			total = 0;
+		}
+	}
+
+	var color = {
+		red: (255 - values[1] + values[4]),
+		green: (values[0] - values[3]),
+		blue: (values[2] - values[5])
+	};
+
+	return color;
+}
+
+function toNumber(color) {
+	var r = parseInt(color.red);
+	var g = parseInt(color.green);
+	var b = parseInt(color.blue);
+
+	var total;
+
+	if (r === 255 && g >= 0 && b === 0) {
+		total = g;
+	}
+
+	if (r >= 0 && g === 255 && b === 0) {
+		total = 255 + (255 - r);
+	}
+
+	if (r === 0 && g === 255 && b >= 0) {
+		total = 255 + 255 + b;
+	}
+
+	if (r === 0 && g >= 0 && b === 255) {
+		total = 255 + 255 + 255 + (255 - g);
+	}
+
+	if (r >= 0 && g === 0 && b === 255) {
+		total = 255 + 255 + 255 + 255 + r;
+	}
+
+	if (r === 255 && g === 0 && b >= 0) {
+		total = 255 + 255 + 255 + 255 + 255 + (255 - b);
+	}
+
+	return total;
+
+}
+
+function check() {
+	for (var x = 0; x < 1530; x++) {
+		var v = (x === toNumber(toColor(x)));
+		if (!v) {
+			console.log(x);
+		}
+	}
+}
